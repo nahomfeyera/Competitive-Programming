9182f5e8d1b54e2927fc0c24aaadf070ae1347de
LidiaS <39269109+LSSYA@users.noreply.github.com>
2022-04-17 20:02:55 +0300

    Create count_lower.cpp

---
+++ count_lower.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums)
+    {
+
+int count=0;
+
+vector<int>ans;
+
+
+	for(int i=0; i<nums.size(); i++)
+	{
+		for(int j=0; j<nums.size(); j++)
+		{
+			if(nums[i]>nums[j])
+			{
+				count++;
+			}
+		}
+		
+		ans.push_back(count);
+        count = 0;
+	}
+	
+	return ans;
+
+    }
+    //O(N2) Time Complexity is The number of input in the array squared.
+};
